2b2cffc270b1ba15fa967eb26a4f8b1dc613c23a
charburgx <charburgx@gmail.com>
2022-12-28 17:15:40 -0600

    docs: anchor in docs url

--- app/util/url.ts
+++ app/util/url.ts
@@ -3,6 +3,6 @@ function joinUrl(...segments: string[]) {
 }
 
 export const PROJECT_REPO = joinUrl(APP_REPOSITORY_URL)
-export const PROJECT_DOCS = joinUrl(PROJECT_REPO)
+export const PROJECT_DOCS = joinUrl(PROJECT_REPO) + "#ponychopper"
 export const PROJECT_ISSUES = joinUrl(PROJECT_REPO, "issues")
 export const PROJECT_RELEASES = joinUrl(PROJECT_REPO, "releases")
